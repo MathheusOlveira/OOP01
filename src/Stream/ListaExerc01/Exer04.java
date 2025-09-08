00cc3566bf4f7a73bc2e429a63f3b136061a7708
MathheusOlveira <thhheusoliveira@gmail.com>
2025-09-08 10:01:36 -0300

    qualquer coisa 02

--- src/Stream/ListaExerc01/Exer04.java
+++ src/Stream/ListaExerc01/Exer04.java
@@ -9,5 +9,8 @@ public class Exer04 {
         Double mediaSaldo = DoubleStream.of(5000, 200, 3000, -4200, 5123, 1500, 1000, 1111)
                 .average()
                 .orElse(0);
+
+
+        System.out.println(" teu cu" + mediaSaldo);
     }
 }
